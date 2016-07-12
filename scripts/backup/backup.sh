83ef871c386a95ec18f96113c0330025013fae5a
Jon Butler <burtnolej@gmail.com>
2016-07-12 07:58:51 -0400

    updated to only create delta backups
    test not updated

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-SOURCE=/usr/
+SOURCE=/usr
 TARGET=/media/backup/test
 PATH=$PATH:/bin:/usr/bin
 BASENAME=`basename "$0"`
 LOCKFILE="."$BASENAME".lock"
 RUNDATE=`date +"%m%d%y-%H%M%S"`
 LOGFILE=$BASENAME.$RUNDATE
-TESTBASE=/mnt/bumblebee-burtnolej/Development/scripts/backup-test-dir
+TESTBASE=/home/burtnolej/Development/pythonapps3/scripts/backup/backup-test-dir
 TESTRESULT=/tmp/$BASENAME.testresult
 MODE=$1
 
@@ -29,18 +29,29 @@ fi
 
 if [ "$MODE" = "test" ]; then
 	SOURCE=$TESTBASE/source/
-	TARGET=$TESTBASE/target
+	TARGET=$TESTBASE/backup
+	BACKUP=./delta
+	echo $RUNDATE > $SOURCE/foobar1 # change a file so delta exists to backup
+	touch $SOURCE/foobar2 # touch a file so delta exists
+
 	EXPTESTRESULT=$TESTBASE/filelist
 	SUCCESSTEST="Files "$EXPTESTRESULT" and "$TESTRESULT" are identical"
-	rm -r $TARGET/*
+
+	EXCLUDE=( "foobar4" "foobar5" )
+	for i in "${EXCLUDE[@]}"
+	do	
+        	EXCLUDEFLAG=$EXCLUDEFLAG" --exclude="$i
+	done
+
 fi
 
-EXEC="rsync -r "$SOURCE" "$TARGET" --links --times -xattr --verbose --log-file="$LOGFILE
-
+EXEC="rsync -r -backup --backup-dir="$BACKUP/$RUNDATE" "$SOURCE" "$TARGET" --links --times -xattr --log-file="$LOGFILE
 `$EXEC`
 
+echo $EXEC >> $LOGFILE # log the command that was run
+
 # check results
-if [ "$1" = "test" ]; then
+if [ "$2" = "checktest" ]; then
 	`ls $TARGET > $TESTRESULT`
 
 	DIFFRESULT=`diff -s $EXPTESTRESULT $TESTRESULT`
@@ -54,4 +65,4 @@ fi
 
 rm $LOCKFILE
 
-tail -n 2 $LOGFILE
+tail -n 3 $LOGFILE
