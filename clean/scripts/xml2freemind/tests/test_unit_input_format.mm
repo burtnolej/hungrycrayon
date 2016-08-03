ce3288542b2822ff15831d89667d4b41fb07cbb8
Jon Butler <burtnolej@googlemail.com>
2016-08-03 07:01:26 -0400

    additional testing files

---
+++ clean/scripts/xml2freemind/tests/test_unit_input_format.mm
@@ -0,0 +1,7 @@
+<root>
+	<Database Name="foobar"/>
+	<Format Name="Database">burgundywhite18</Format>
+        <Format Name="Table">greyblack18</Format>
+        <Format Name="Column">darkbluelightblue16</Format>
+        <Format Name="Row">yellowburgundy14</Format>
+</root>
