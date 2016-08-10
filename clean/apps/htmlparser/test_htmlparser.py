a79575ef22f0a881f1dc04649a62c8801ea8fba7
Jon Butler <burtnolej@gmail.com>
2016-08-10 14:28:41 -0400

    add in tests to html parser to make sure database persist work correctly

--- clean/apps/htmlparser/test_htmlparser.py
+++ clean/apps/htmlparser/test_htmlparser.py
@@ -4,7 +4,7 @@ from misc_utils_generic import GenericBase
 from misc_utils_enum import enum
 from misc_utils_objectfactory import ObjFactory
 from database_table_util import dbtblgeneric
-from database_util import Database
+from database_util import Database, tbl_create
 import unittest
 
 period_enum = ['8:30-9:10','9:11-9:51','9:52-10:32','10:33-11:13',
@@ -35,7 +35,7 @@ class Teacher(dbtblgeneric):
     def __repr__(self):
         return(self.objid)
 
-class Test_ObjFrameworkBasic(unittest.TestCase):
+class Test_HTMLParserBasic(unittest.TestCase):
 
     def setUp(self):
         self.of = ObjFactory(True)
@@ -85,8 +85,91 @@ class Test_ObjFrameworkBasic(unittest.TestCase):
         self.assertEquals(_lesson.student,'booker')
         self.assertEquals(_lesson.period,2)
         self.assertEquals(_lesson.dow,3)
+        
+class Test_HTMLParserPersist(unittest.TestCase):
+
+    def setUp(self):
+        self.of = ObjFactory(True)
+        self.database = Database('foobar')
+        lesson=self.of.new('Lesson',
+                           objid='lesson0',
+                           constructor='datamembers',
+                           database=self.database,
+                           modname=__name__,
+                           dm={'student':'\"booker\"',
+                               'period':2,
+                               'dow':3})
+
+        lesson.attr_set(self.of,self.database,'\"fran\"','Teacher')
+        lesson.type="psych"
+        
+    def test_persist(self):
+        
+        for obj in self.of.object_iter():
+            obj.persist()
+            
+        with self.database:
+            self.assertEquals([['booker']],self.database.execute("select student from Lesson"))
+            self.assertEquals([['fran']],self.database.execute("select name from Teacher"))
+            
+    def test_persist(self):
+        
+        for obj in self.of.object_iter():
+            obj.persist()
+            
+        with self.database:
+            self.assertEquals([['booker']],self.database.execute("select student from Lesson"))
+            self.assertEquals([['fran']],self.database.execute("select name from Teacher"))
+        
+class Test_HTMLParserPersistScale(unittest.TestCase):
+
+    def setUp(self):
+        self.of = ObjFactory(True)
+        self.database = Database('foobar',True)
+        
+        with self.database:
+            tbl_create(self.database,'Lesson',[('student','text'),
+                                               ('period','integer'),
+                                               ('dow','integer'),
+                                               ('__timestamp','text'),
+                                               ('__id','text')])
+            
+            tbl_create(self.database,'Teacher',[('name','text'),
+                                               ('__timestamp','text'),
+                                               ('__id','text')])
+
+         
+        
+        self.teachers = ['fran','dave','brian','steve','paul','damian','roger','jon','graeme','ringo']
+        for day in range(5):
+            for period in range(10):
+                lesson=self.of.new('Lesson',
+                                   objid='lesson'+str(day)+str(period),
+                                   constructor='datamembers',
+                                   database=self.database,
+                                   modname=__name__,
+                                   dm={'student':'\"booker\"',
+                                       'period':period,
+                                       'dow':day})
+
+                lesson.attr_set(self.of,self.database,'\"'+self.teachers[period]+'\"','Teacher')
+                lesson.type="psych"
+                
+                
+    def test(self):
+        for obj in self.of.object_iter():
+            obj.persist(False)
+            
+        with self.database:
+            self.assertEquals(self.database.execute("select count(*) from Lesson",True),50)
+            self.assertEquals(self.database.execute("select count(*) from Teacher",True),10)  
+        
     
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite = unittest.TestLoader().loadTestsFromTestCase(Test_ObjFrameworkBasic)
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_HTMLParserBasic))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_HTMLParserPersist))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_HTMLParserPersistScale))
+    
+    
     unittest.TextTestRunner(verbosity=2).run(suite)
