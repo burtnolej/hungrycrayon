57c9d06e37a58afd4b3076677aaaa60bc219b34a
Jon Butler <burtnolej@gmail.com>
2016-09-06 06:59:56 -0400

    first version of data stuff

---
+++ clean/scripts/usda-nutrientdb/examples.sh
@@ -0,0 +1,5 @@
+#curl -H "Content-Tyation/xml" -d '<report><ndbno>19816</ndbno><type>f</type></report>' ZJjApBRrcjlz1dd3fw1PAoQghya6ia8GgCBUu7cC@api.nal.usda.gov/usda/ndb/reports 
+#curl -H "Content-Tyation/xml" -d '<report><ndbno>01009</ndbno><type>f</type></report>' ZJjApBRrcjlz1dd3fw1PAoQghya6ia8GgCBUu7cC@api.nal.usda.gov/usda/ndb/reports 
+
+curl -H "Content-Tyation/xml" -d '<report><ndbno>01009</ndbno><type>f</type></report>' DEMO_KEY@api.nal.usda.gov/usda/ndb/reports 
+
