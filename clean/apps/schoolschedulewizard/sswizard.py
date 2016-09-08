47958319465b41895d2ec533b5572312b9a132e6
Jon Butler <burtnolej@gmail.com>
2016-09-08 15:47:53 -0400

    persist to db working;reload partially added 2nd screen to show student totals

--- clean/apps/schoolschedulewizard/sswizard.py
+++ clean/apps/schoolschedulewizard/sswizard.py
@@ -12,7 +12,7 @@ from misc_utils import Log
 from misc_utils_objectfactory import ObjFactory
 
 from database_util import Database, tbl_create
-from database_table_util import dbtblgeneric
+from database_table_util import dbtblgeneric, tbl_rows_get
 
 #from schoolscheduler_utils import *
 
@@ -30,6 +30,8 @@ types_enum = ['period','day','other','psych','edu',
 period_enum = ['8:30-9:10','9:11-9:51','9:52-10:32','10:33-11:13',
                '11:13-11:45','11:45-12:25','12:26-1:06','1:07-1:47',
                '1:48-2:28','2:30-3:00']
+period_map = dict((pe,period_enum.index(pe)) for pe in period_enum)
+
 day_enum = ['Monday','Tuesday','Wednesday','Thursday','Friday']
 other_enum = ['MOVEMENT','CORE','YOGA','MUSIC','CODING/TED TALKS',
               'REGENTS PREP','REGIONS PREP','READING PERIOD',
@@ -50,12 +52,16 @@ student_enum = ['NATHANIEL','TRISTAN','SIMON A.','ORIG','COBY',
                 'ASHLEY','YOSEF','LUCY','JAKE','ASHER',
                 'DONOVAN','LIAM','SIMON B','NICK']
 
-teacher_lesson_type = {'MELISSA':'psych','EMILY':'psych','ALEXA':'other','ASHLEY':'other',
-                       'DYLAN':'other','FRAN':'other','ISAAC':'other','KAYLA':'other',
-                       'MOIRA':'other','NATHANIEL':'other','RACHEL':'other','RAHUL':'other',
-                       'RICKY':'other','TRISTAN':'other','YOSEF':'other','CHRIS':'other'}
+teacher_lesson_type = {"Stan":"","Galina":"","Samantha":"","Amelia":"","Paraic":""}
+
+#teacher_lesson_type = {'MELISSA':'psych','EMILY':'psych','ALEXA':'other','ASHLEY':'other',
+#                       'DYLAN':'other','FRAN':'other','ISAAC':'other','KAYLA':'other',
+#                       'MOIRA':'other','NATHANIEL':'other','RACHEL':'other','RAHUL':'other',
+#                       'RICKY':'other','TRISTAN':'other','YOSEF':'other','CHRIS':'other'}
 
 teacher_enum = teacher_lesson_type.keys()
+
+teacher_map = dict((te,teacher_enum.index(te)) for te in teacher_enum)
 
 log = Log()
 
@@ -81,19 +87,25 @@ class schoolschedgeneric(dbtblgeneric):
         super(schoolschedgeneric,self).__init__(**kwargs)
         self.objid = objid
 
+        
         for k,v in kwargs['dm'].iteritems():
             if v <> 'None':
                 self.attr_set(v,k)
                 
     def attr_set(self,name,clsname):
+        
         datamembers = _initdatamembers(clsname,name=name)
         setattr(self,clsname,_obj_create(datamembers,self.database,self.of,clsname))
+            
         return(getattr(self,clsname))
     
     def __repr__(self):
         return(self.objid)
     
 class teacher(schoolschedgeneric):
+    pass
+
+class saveversion(schoolschedgeneric):
     pass
 
 class lessontype(schoolschedgeneric):
@@ -142,9 +154,10 @@ def _initdatamembers(clsname,**kw):
             raise KeyError("lesson init requires args: schedule_num, day_num, period_num, student_num")
         
         dm = {'teacher':kw['teacher'],
+              'saveversion':kw['saveversion'],
               'subject':'None',
               'lessontype':'None',
-              'period':period_enum[kw['period_num']],
+              'period':kw['period_num'],
               'dow':day_enum[kw['day_num']],
               'student':student}
     else:
@@ -163,7 +176,8 @@ def _initdatamembers(clsname,**kw):
 class WizardUI(object):
     def __init__(self,master):
 
-    
+        self.lastsaveversion=0
+        
         self.master = master
         self.maxrows=14 # rows in the grid
         self.maxcols=20 # cols in the grid
@@ -202,13 +216,30 @@ class WizardUI(object):
         controlpanel = Frame(master)
         controlpanel.grid(row=2)
         
-        self.save_button = Button(controlpanel,command=self.persist,text="persist",name="svb")
+        self.save_button = Button(controlpanel,command=self.save,text="save",name="svb")
         self.save_button.grid(row=2,column=0)
         self.save_button.focus_get()
         
-        self.balance_button = Button(controlpanel,command=self.persist,text="balance",name="bb")
-        self.balance_button.grid(row=2,column=1)
-        self.balance_button.focus_get()
+        self.persist_button = Button(controlpanel,command=self.persist,text="persist",name="pb")
+        self.persist_button.grid(row=2,column=1)
+        self.persist_button.focus_get()
+        
+        self.spacer_label = Label(controlpanel,text="                      ")
+        self.spacer_label.grid(row=2,column=2)
+        
+        self.dbload_entry_label = Label(controlpanel,text="version")
+        self.dbload_entry_label.grid(row=2,column=3)
+        self.dbload_entry_label.focus_get()
+        self.dbload_entry_sv = StringVar()
+        self.dbload_entry = Entry(controlpanel,textvariable=self.dbload_entry_sv)
+        self.dbload_entry.grid(row=2,column=4)
+        self.dbload_entry.focus_get()
+        
+        self.dbload_button = Button(controlpanel,command=self.load,text="dbload",name="dbl")
+        self.dbload_button.grid(row=2,column=5)
+        self.dbload_button.focus_get()
+        
+        
         
         self.bgmaxrows=len(period_enum)+1
         self.bgmaxcols=len(student_enum)+1 
@@ -236,24 +267,15 @@ class WizardUI(object):
             self.balancegrid.widgets[0][y].config(text=student_enum[y-1])
         
         
-    def persist(self):
+    def save(self):
 
+        of.reset()
+        
         for x in range(1,self.maxrows):
             ylabel=self.entrygrid.widgets[x][0].sv.get()
-
-            try:
-                ylabel = int(ylabel)
-            except:
-                pass
             
             for y in range(1,self.maxcols):
                 xlabel=self.entrygrid.widgets[0][y].sv.get()
-                
-                try:
-                    xlabel = int(xlabel)
-                except:
-                    pass
-
                 
                 value =  self.entrygrid.widgets[x][y].sv.get()
                 
@@ -268,27 +290,33 @@ class WizardUI(object):
                     datamembers = _initdatamembers('lesson',
                                                    schedule_num = '1',
                                                    day_num=1, 
-                                                   period_num=ylabel-1,
+                                                   period_num=ylabel,
                                                    student_num=value,
+                                                   saveversion=self.lastsaveversion,
                                                    teacher=xlabel)
                     
                     _lesson_create(datamembers,database,of)
                     
-                    current_value = self.balancegrid.widgets[ylabel][value].cget("text")
+                    # get the actual row number from the period string
+                    # add one to account for the origin
+                    x = period_map[ylabel]+1
+                    
+                    current_value = self.balancegrid.widgets[x][value].cget("text")
                     if current_value == xlabel:
                         pass
                     elif current_value == "":
                         
-                        self.balancegrid.widgets[ylabel][value].config(background='lightgreen')
-                        self.balancegrid.widgets[ylabel][value].config(text=xlabel)
+                        self.balancegrid.widgets[x][value].config(background='lightgreen')
+                        self.balancegrid.widgets[x][value].config(text=xlabel)
                     else:
-                        self.balancegrid.widgets[ylabel][value].config(text=current_value+","+xlabel)
-                        self.balancegrid.widgets[ylabel][value].config(background='red')
+                        self.balancegrid.widgets[x][value].config(text=current_value+","+xlabel)
+                        self.balancegrid.widgets[x][value].config(background='red')
                     
-            self.student_totals_calc()            
-                    
-        schedule_persist(of,database)
-
+        self.student_totals_calc()        
+            
+        self.dbload_entry_sv.set(self.lastsaveversion)
+            
+        self.lastsaveversion+=1
 
     def student_totals_calc(self):
         
@@ -310,31 +338,37 @@ class WizardUI(object):
             self.save_button.focus_set()
         return("break")
     
-    def inject_values(self,values=None):
+    def load(self,values=None):
+        
         
         if values==None:
             #load from database
-            pass
-            
-        for x in range(len(values)):
-            for y in range(len(values[0])):
-                self.entrygrid.widgets[x][y].sv.set(values[x][y])
+            with database:
+                colndefn,rows = tbl_rows_get(database,'lesson',['period','student','teacher','dow'],
+                                    ('saveversion',str(self.lastsaveversion)))
+            print rows
+        else:
+                
+            for x in range(len(values)):
+                for y in range(len(values[0])):
+                    self.entrygrid.widgets[x][y].sv.set(values[x][y])
 
-def schedule_persist(of,database):
-    with database:
-        for obj in of.object_iter():
-            obj.persist()
+    def persist(self):
+        with database:
+            for obj in of.object_iter():
+                obj.persist()
 
     
 if __name__ == "__main__":
     master = Tk()
+    
     
     database = Database('htmlparser')
     of = ObjFactory(True)
     
     ui = WizardUI(master)
     values=[["","Stan","Galina","Samantha","Amelia","Paraic"],
-            [1,1,9,3,5,7],
-            [2,2,1,4,6,8]]
-    ui.inject_values(values)
+            ['8:30-9:10',1,9,3,5,7],
+            ['9:11-9:51',2,1,4,6,8]]
+    ui.load(values)
     master.mainloop()
