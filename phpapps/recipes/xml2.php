dcfd3f737d04d7d6f7ebd928754eadf51a05bf38
Jon Butler <burtnolej@googlemail.com>
2016-11-02 18:40:22 -0400

    updated xml recipe

--- phpapps/recipes/xml2.php
+++ phpapps/recipes/xml2.php
@@ -1,54 +1,103 @@
+<html>
+<style>
+
+#cell {
+    padding:0px;
+    line-height:30px;
+    background-color:#eeeeee;
+    //<!height:2000px;>
+    float:left;
+    text-align:center;;
+    border: 1px solid #73AD21;
+}
+
+#rowhdrcell {
+    padding:0px;
+    line-height:30px;
+    foreground-color:#D0C978;
+    background-color:#98969B;
+    <!height:2000px;>
+    float:left;
+    text-align:center;;
+    width: 50px;
+    border: 1px solid #73AD21;
+}
+
+#table {
+	width:100%;
+   background-color:#eeeeee;
+   	<!height:2000px;>
+   float:left;
+   text-align:center;;
+}
+
+table {
+    border-collapse: collapse;
+}
+
+</style>
+</html>
+
 <?php
-$xmlstr = <<<XML
-<root>
-	<row id='1'>
-		<cell id = '1.1'>
-			<value>foo</value>
-			<fgcolor>FF0FFF</fgcolor>
-			<bgcolor>FF00FF</bgcolor>
-		</cell>
-	</row>
-	<row id='2'>
-		<cell id = '2.1'>
-			<value>bar</value>
-			<fgcolor>FFFFFF</fgcolor>
-			<bgcolor>FF0000</bgcolor>
-		</cell>
-	</row>
-</root>
-XML;
 
 include_once '../utils/utils_xml.php';
 include_once '../utils/utils_error.php';
 include_once '../utils/utils_test.php';
+include_once 'test_xml2.php';
 
 set_error_handler('\\UtilsError::error_handler');
 
 $gridarr = Array(); // declare resulting array
 
 // load xml string into XML Utils
-$utilsxml = simplexml_load_string($xmlstr, 'utils_xml');		
+$utilsxml = simplexml_load_string($xmlstr_rowhdr, 'utils_xml');		
 
 // get a list of all rows
 $_rows = $utilsxml->xpath("//row");
 
-echo "<table border='1' style=width:100%>";
+echo "<table id=table >";
+
+function drawcell($cell) {
+	
+	if (isset($cell->type)) {
+		echo "<td id=".$cell->type;
+	}
+	else {
+		echo "<td id=cell";
+	}
+	
+	echo " bgcolor=#".$cell->bgcolor;
+	echo " fgcolor=#".$cell->fgcolor;
+	echo ">";
+	echo $cell->value;
+	echo "</td>";
+} 
 
 foreach ($_rows as $_row) {
 	
-	echo "<tr>";
-	
-	// get a list of the cells (children) of this row
-	$_cells = $_row->xpath("child::*");
+	echo "<tr>"; // start an html row
+	$_cells = $_row->xpath("child::*"); // get a list of the cells (children) of this row
 	
 	foreach ($_cells as $_cell) {
-
-		echo "<td";
-		echo " bgcolor=#".$_cell->bgcolor;
-		echo " fgcolor=#".$_cell->fgcolor;
-		echo ">";
-		echo $_cell->value;
-		echo "</td>";
+		
+		$_subcells = $_cell->xpath("child::subcell"); // see if any subcells exist
+	
+		if (sizeof($_subcells) <> 0) {
+			
+				echo "<td>";
+				echo "<table id=table>"; // start a new table
+				echo "<tr>"; // all subcells go on one row
+				
+				foreach ($_subcells as $_subcell) {
+						drawcell($_subcell);
+				}
+				echo "</tr>";
+				echo "</table>";
+				echo "</td>";
+		}
+		else { // create a regular cell
+			drawcell($_cell);
+		}
 	}
 	echo "</tr>";
 }
