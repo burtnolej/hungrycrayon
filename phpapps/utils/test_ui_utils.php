dd4cd42d77e63136887e20f3b85b103760deee4e
Jon Butler <burtnolej@gmail.com>
2016-11-17 10:50:44 -0500

    test for env and use env for autoload

--- phpapps/utils/test_ui_utils.php
+++ phpapps/utils/test_ui_utils.php
@@ -1,9 +1,13 @@
 <?php
 
+$PHPLIBPATH = getenv("PHPLIBPATH");
+	
+if ($PHPLIBPATH == "") {
+	trigger_error("Fatal error: env PHPLIBPATH must be set", E_USER_ERROR);
+}
 
-require_once __DIR__ . '/composer/vendor/autoload.php';
-
-set_include_path('/home/burtnolej/Development/pythonapps3/phpapps/utils/');
+set_include_path($PHPLIBPATH);
+require_once 'autoload.php';
 
 include_once 'ui_utils.php';
 include_once 'utils_error.php';
@@ -21,17 +25,16 @@ class test_gethtmldropdown extends PHPUnit_Framework_TestCase
 		
 		ob_start(); 
 		
-		$this->expected_result = '<label for="foobar" >foobar</label><input type="text" name="foobar" id="foobar" list="suggestions0" /><datalist id="suggestions0"><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></datalist>';
+		$this->expected_result = '<label for="foobar" >foobar</label><input type="text" name="foobar" id="foobar" list="suggestions0"><datalist id="suggestions0"><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></datalist>';
 				
 		$_result = gethtmldropdown($column,$values,$widgetcount);
 					
 		$result = ob_get_contents();
 		ob_end_clean();		
-		$this->assertEquals($result,$this->expected_result);
+		$this->assertEquals($this->expected_result,$result);
 	}
 }
-$stf = new test_gethtmldropdown();
-$stf->test_();
+
 
 class test_gethtmldbdropdown extends PHPUnit_Framework_TestCase
 {
@@ -42,7 +45,7 @@ class test_gethtmldbdropdown extends PHPUnit_Framework_TestCase
 
 		ob_start(); 
 		
-		$this->expected_result = '<div class="container">	<label for="subject" >subject</label><input type="text" name="subject" id="subject" list="suggestions0" /><datalist id="suggestions0"><option>Math</option><option>ELA</option><option>Engineering</option></datalist></div>';
+		$this->expected_result = '<div class="container"><label for="subject" >subject</label><input type="text" name="subject" id="subject" list="suggestions0"><datalist id="suggestions0"><option>Math</option><option>ELA</option><option>Engineering</option></datalist></div>';
 						
 		gethtmldbdropdown($dbname,$tablename);
 					
@@ -52,8 +55,7 @@ class test_gethtmldbdropdown extends PHPUnit_Framework_TestCase
 	}
 }
 
-$stf = new test_gethtmldbdropdown();
-$stf->test_();
+
 
 class test_gethtmlbutton extends PHPUnit_Framework_TestCase
 {
@@ -74,8 +76,7 @@ class test_gethtmlbutton extends PHPUnit_Framework_TestCase
 	}
 }
 
-$stf = new test_gethtmlbutton();
-$stf->test_();
+
 
 class test_gethtmlxmldropdown extends PHPUnit_Framework_TestCase
 {
@@ -85,7 +86,7 @@ class test_gethtmlxmldropdown extends PHPUnit_Framework_TestCase
 
 		ob_start(); 
 		
-		$this->expected_result = '<div class="container">	<label for="xaxis" >xaxis</label><input type="text" name="xaxis" id="xaxis" list="suggestions0" /><datalist id="suggestions0"><option>period</option><option>dow</option><option>adult</option><option>subject</option></datalist></div><div class="container">	<label for="yaxis" >yaxis</label><input type="text" name="yaxis" id="yaxis" list="suggestions1" /><datalist id="suggestions1"><option>period</option><option>dow</option><option>adult</option><option>subject</option></datalist></div>';
+		$this->expected_result = '<div class="container"><label for="xaxis" >xaxis</label><input type="text" name="xaxis" id="xaxis" list="suggestions0"><datalist id="suggestions0"><option>period</option><option>dow</option><option>adult</option><option>subject</option></datalist></div><div class="container"><label for="yaxis" >yaxis</label><input type="text" name="yaxis" id="yaxis" list="suggestions1"><datalist id="suggestions1"><option>period</option><option>dow</option><option>adult</option><option>subject</option></datalist></div>';
 										
 		gethtmlxmldropdown($xml);
 					
@@ -96,8 +97,7 @@ class test_gethtmlxmldropdown extends PHPUnit_Framework_TestCase
 	}
 }
 
-$stf = new test_gethtmlxmldropdown();
-$stf->test_();
+
 
 class test_gethtmlmultiselect extends PHPUnit_Framework_TestCase
 {
@@ -107,20 +107,36 @@ class test_gethtmlmultiselect extends PHPUnit_Framework_TestCase
 		$query = "select name from sqlite_master";
 		$name = "ingredients";
 
-		//ob_start(); 
+		ob_start(); 
 		
-		$this->expected_result = '<div class="container">	<label for="xaxis" >xaxis</label><input type="text" name="xaxis" id="xaxis" list="suggestions0" /><datalist id="suggestions0"><option>period</option><option>dow</option><option>adult</option><option>subject</option></datalist></div><div class="container">	<label for="yaxis" >yaxis</label><input type="text" name="yaxis" id="yaxis" list="suggestions1" /><datalist id="suggestions1"><option>period</option><option>dow</option><option>adult</option><option>subject</option></datalist></div>';
-										
+		$this->expected_result = '<input id="adult" type="checkbox" name="ingredients[]" value="adult"/><label for="adult" >adult</label><br><input id="student" type="checkbox" name="ingredients[]" value="student"/><label for="student" >student</label><br><input id="period" type="checkbox" name="ingredients[]" value="period"/><label for="period" >period</label><br><input id="dow" type="checkbox" name="ingredients[]" value="dow"/><label for="dow" >dow</label><br><input id="lessontype" type="checkbox" name="ingredients[]" value="lessontype"/><label for="lessontype" >lessontype</label><br><input id="subject" type="checkbox" name="ingredients[]" value="subject"/><label for="subject" >subject</label><br><input id="synonyms" type="checkbox" name="ingredients[]" value="synonyms"/><label for="synonyms" >synonyms</label><br><input id="recordtype" type="checkbox" name="ingredients[]" value="recordtype"/><label for="recordtype" >recordtype</label><br><input id="session" type="checkbox" name="ingredients[]" value="session"/><label for="session" >session</label><br><input id="lesson" type="checkbox" name="ingredients[]" value="lesson"/><label for="lesson" >lesson</label><br>';
+												
 		gethtmlmultiselect($dbname,$query,$name);
 					
-		//$result = ob_get_contents();
-		//ob_end_clean();
+		$result = ob_get_contents();
+		ob_end_clean();
 	
-		//$this->assertEquals($result,$this->expected_result);
+		$this->assertEquals($result,$this->expected_result);
 	}
 }
 
-$stf = new test_gethtmlmultiselect();
+$stf = new test_gethtmldropdown();
 $stf->test_();
 
+
+
+$stf = new test_gethtmldbdropdown();
+$stf->test_();
+
+
+
+$stf = new test_gethtmlxmldropdown();
+$stf->test_();
+
+
+$stf = new test_gethtmlbutton();
+$stf->test_();
+
+$stf = new test_gethtmlmultiselect();
+$stf->test_();
 ?>
