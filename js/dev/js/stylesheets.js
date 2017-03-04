668e6756453277eadd432a2ae2eb37af41b1cc0d
Jon Butler <burtnolej@googlemail.com>
2017-03-04 15:39:48 -0500

    moved stylesheets.js

---
+++ js/dev/js/stylesheets.js
@@ -0,0 +1,13 @@
+/*requirejs.config({
+    baseUrl: 'js/lib',
+    paths: {
+       app: '../app'
+    }
+});*/
+
+requirejs(['jquery'],
+function   ($) {
+	
+	$('head').html('<link rel="stylesheet" type="text/css" href="css/select.css" /><link rel="stylesheet" type="text/css" href="css/div.css" /><link rel="stylesheet" type="text/css" href="css/switch.css" /><link rel="stylesheet" type="text/css" href="css/menu.css" />');
+	}
+);
